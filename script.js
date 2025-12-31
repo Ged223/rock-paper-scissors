b954ec672b002efbcbe0623b4e120f0cef7b6883
Gedjur <gedsvk@gmail.com>
2025-12-31 11:36:04 +0100

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -14,5 +14,30 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     const humanChoice = prompt("rock, paper, or scissors?");
-    return humanChoice;
+    return humanChoice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie! You made the same choice.")
+        return;
+    } else if (humanChoice === "rock" && computerChoice === "paper"){
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose! Scissors beat Paper");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors beat Paper");
+        humanScore++;
+    }
 }
